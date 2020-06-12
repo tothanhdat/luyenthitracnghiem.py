cb8caacea8bc2049a5b7448bb6408820fde35b47
To Dat <todat999@gmail.com>
2020-06-13 00:48:18 +0700

    ok

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ app.use(function(req, res, next){
     res.status(404).render('pages/page-404', {title: "Sorry, page not found"});
 });
 
-const uri = 'mongodb://localhost/luyenthitracnghiempy';
-//const uri = 'mongodb://datchen:datchen123@ds261238.mlab.com:61238/lttn-py';
+//const uri = 'mongodb://localhost/luyenthitracnghiempy';
+const uri = 'mongodb://datchen:datchen123@ds261238.mlab.com:61238/lttn-py';
 const PORT = process.env.PORT || 3000;
 
 mongoose.set('useCreateIndex', true); //ẩn cảnh báo
